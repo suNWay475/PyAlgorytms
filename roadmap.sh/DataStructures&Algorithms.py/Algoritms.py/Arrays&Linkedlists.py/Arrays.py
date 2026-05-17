f2bc033a053bd2bf425400a82840175809e253c1
Vitalii Bukshan <sway6432@gmail.com>
2026-05-17 20:14:27 +0300

    Update dor Arrays

--- roadmap.sh/DataStructures&Algorithms.py/Algoritms.py/Arrays&Linkedlists.py/Arrays.py
+++ roadmap.sh/DataStructures&Algorithms.py/Algoritms.py/Arrays&Linkedlists.py/Arrays.py
@@ -10,17 +10,69 @@
 # print(arr) # print all arr
 
 
+# def sum_evens(arr):
+#     total = 0
+#     for i in arr:
+#         if i % 2 == 0:
+#             total += i
+#     return total
+
+# arr = [1 , 2, 3, 4 ,5  ,6 ,7 ,8 ,9 , 0, 10 ,11, 12, 10 , 120 ,130]
+# print(sum_evens(arr))
 
 
 
+# def two_sum(arr, target):
+#     left = 0 
+#     right = len(arr) - 1 
+
+#     while left < right:
+#         current_sum = arr[left] + arr[right]
+#         if current_sum == target:
+#             return [left , right]
+#         elif current_sum < target:
+#             left += 1
+#         else:
+#             right -= 1
+        
+
+# print(two_sum([1, 2, 3, 4, 6], 10))
+
+# def reverse_array(arr):
+#     left = 0
+#     right = len(arr) - 1
+
+#     while left < right:
+#         arr[left], arr[right] = arr[right], arr[left]
+#         left += 1
+#         right -= 1
+
+#     return arr
+
+   
 
 
-def sum_evens(arr):
-    total = 0
+# print(reverse_array([1, 2, 3, 4, 5]))  # [5, 4, 3, 2, 1]
+
+
+# видали всі дублікати з відсортованого списку
+def remove_duplicates(arr):
+    result = []
+    
     for i in arr:
-        if i % 2 == 0:
-            total += i
-    return total
+        if i not in result:
+            result.append(i)
+    
+    return result
 
-arr = [1 , 2, 3, 4 ,5  ,6 ,7 ,8 ,9 , 0, 10 ,11, 12, 10 , 120 ,130]
-print(sum_evens(arr))
+print(remove_duplicates([1, 1, 2, 2, 3, 4, 4, 5]))  
+#
+def remove_duplicates2(arr):
+    left = 0 
+    for right  in range(1 , len(arr)):
+        if arr[right] != arr[left]:
+            left += 1
+            arr[left] = arr[right]
+            
+    return arr[:left + 1]
+print(remove_duplicates2([1, 1, 2, 2, 3, 4, 4, 5]))
